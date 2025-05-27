ac732f3b6da1651aa2a1d5ebddd7be93b8e75230
Edward Jin <ejscfj@gmail.com>
2025-05-27 10:43:20 -0700

    added two tests

--- tests/raft_leader_election_test.go
+++ tests/raft_leader_election_test.go
@@ -35,6 +35,10 @@ func checkAllStatus(nodes []*raft.RaftNode, interval time.Duration, done chan st
 				statuses := make([]TestNodeStatus, len(nodes))
 
 				for i, node := range nodes {
+					if node == nil {
+						continue
+					}
+
 					// we need to lock the node to safely read its status
 					node.Mu.Lock()
 
@@ -61,8 +65,10 @@ func checkAllStatus(nodes []*raft.RaftNode, interval time.Duration, done chan st
 	return statusChan
 }
 
-// waitForStableLeader waits for the cluster to have a stable leader
-// returns id of the leader if found else returns -1
+/*
+waitForStableLeader waits for the cluster to have a stable leader
+returns id of the leader if found else returns -1 
+*/
 func waitForStableLeader(statusChan chan []TestNodeStatus, timeout time.Duration) int {
 	deadline := time.After(timeout)
 	stableCount := 0
@@ -108,7 +114,7 @@ func waitForStableLeader(statusChan chan []TestNodeStatus, timeout time.Duration
 	}
 }
 
-// Tests if leadership can be established within 10 seconds with 3 Raft nodes (no crashes)
+// Test if leadership can be established within 10 seconds 
 func TestLeaderElection(t *testing.T) {
 	fmt.Println("Running:", t.Name())
 
@@ -146,11 +152,10 @@ func TestLeaderElection(t *testing.T) {
 	for i := 0; i < 3; i++ {
 		close(nodes[i].Shutdown)
 	}
-
 }
 
-// Tests if leadership can be established after node failure
-func TestFailElection(t *testing.T) {
+// Test if leadership can be established after leader fails
+func TestLeaderFailElection(t *testing.T) {
 	fmt.Println("Running:", t.Name())
 
 	os.Setenv("RAFT_HEARTBEAT_INTERVAL", "500")
@@ -182,15 +187,18 @@ func TestFailElection(t *testing.T) {
 	}
 
 	fmt.Printf("Previous leader: %d\n", previousLeader)
+
 	// shutdown the previous leader
 	close(nodes[previousLeader].Shutdown)
 
 	// wait for shutdown
 	time.Sleep(2 * raft.DefaultRPCTimeout * time.Second)
+
 	if leader := waitForStableLeader(statusUpdates, 10*time.Second); leader == -1 || leader == previousLeader {
 		t.Errorf("FAILURE: no leader or previous leader is still active after shutdown %d", leader)
 	}
-	// close each backend
+
+	// close each backend server
 	for i := range 3 {
 		if i != previousLeader {
 			close(nodes[i].Shutdown)
@@ -198,7 +206,7 @@ func TestFailElection(t *testing.T) {
 	}
 }
 
-// test that node joining the cluster becomes a follower
+// Test if a node joining the cluster becomes a follower 
 func TestJoinCluster(t *testing.T) {
 	fmt.Println("Running:", t.Name())
 
@@ -214,9 +222,9 @@ func TestJoinCluster(t *testing.T) {
 	numNodes := len(peers)
 	nodes := make([]*raft.RaftNode, numNodes)
 	shutdownChans := make([]chan struct{}, numNodes)
-	initialNodeCount := numNodes - 1 // N-1 initial nodes
+	initialNodeCount := numNodes - 1 
 
-	// leave one out
+	// leave one node out intentionally
 	initialNodesSlice := make([]*raft.RaftNode, initialNodeCount)
 	for i := range initialNodesSlice {
 		id := int32(i)
@@ -226,7 +234,6 @@ func TestJoinCluster(t *testing.T) {
 		go utils.ServeBackend(id, peers, shutdownChans[i], nodes[i])
 	}
 
-	// Start status checking for the initial N-1 nodes
 	initialStatusDone := make(chan struct{})
 	initialStatusUpdates := checkAllStatus(initialNodesSlice, 100*time.Millisecond, initialStatusDone)
 
@@ -242,7 +249,7 @@ func TestJoinCluster(t *testing.T) {
 
 	close(initialStatusDone)
 
-	// Start the Nth node (the joining node, ID 2)
+	// now we have the left out node join
 	joiningNodeID := int32(numNodes - 1)
 	fmt.Printf("Starting joining node: %d\n", joiningNodeID)
 	shutdownChans[joiningNodeID] = make(chan struct{})
@@ -252,7 +259,7 @@ func TestJoinCluster(t *testing.T) {
 	// wait for the joining node to start
 	time.Sleep(2 * time.Second)
 
-	// Start status checking for all N nodes
+	// start status checking for all nodes
 	allNodesStatusDone := make(chan struct{})
 	allNodesStatusUpdates := checkAllStatus(nodes, 100*time.Millisecond, allNodesStatusDone)
 
@@ -284,7 +291,7 @@ CheckLoop:
 				}
 			}
 
-			if joinedNodeStatus.ID == joiningNodeID && finalLeaderStatus.ID == int32(finalLeaderID) { // Ensure both statuses are updated
+			if joinedNodeStatus.ID == joiningNodeID && finalLeaderStatus.ID == int32(finalLeaderID) { 
 				if joinedNodeStatus.State == raft.Follower && joinedNodeStatus.Term == finalLeaderStatus.Term {
 					break CheckLoop
 				}
@@ -307,12 +314,135 @@ CheckLoop:
 			finalLeaderStatus.Term, initialLeaderTerm)
 	}
 
-	// Cleanup
+	// cleanup
 	close(allNodesStatusDone)
 	fmt.Println("Shutting down nodes...")
 	for i := range numNodes {
 		if shutdownChans[i] != nil {
 			close(shutdownChans[i])
+		}
+	}
+}
+
+// Test no leader elected if minority nodes alive
+func TestNoLeaderWithMinorityNodes(t *testing.T) {
+	fmt.Println("Running:", t.Name())
+
+	os.Setenv("RAFT_HEARTBEAT_INTERVAL", "500")
+	os.Setenv("RAFT_ELECTION_TIMEOUT_MIN", "1000")
+	os.Setenv("RAFT_ELECTION_TIMEOUT_MAX", "2000")
+
+	peers := map[int32]string{
+		1: "localhost:50051",
+		2: "localhost:50052",
+		3: "localhost:50053",
+	}
+
+	nodes := make([]*raft.RaftNode, 3)
+
+	// only start 1 node
+	shutdown := make(chan struct{})
+	nodes[0] = raft.NewRaftNode(1, peers, shutdown)
+	go utils.ServeBackend(1, peers, shutdown, nodes[0])
+
+	nodes[1] = nil
+	nodes[2] = nil
+
+	// start status checking
+	statusChan := make(chan struct{})
+	statusUpdates := checkAllStatus(nodes, 100*time.Millisecond, statusChan)
+
+	// wait then verify no leader is elected in 15 seconds
+	timer := time.NewTimer(15 * time.Second)
+	defer timer.Stop()
+
+	leaderElected := false
+loop:
+	for {
+		select {
+		case statuses := <-statusUpdates:
+			for _, status := range statuses {
+				if status.IsLeader {
+					leaderElected = true
+					break
+				}
+			}
+		case <-timer.C:
+			break loop
+		}
+	}
+
+	close(statusChan)
+	close(nodes[0].Shutdown)
+
+	if leaderElected {
+		t.Error("FAIL: A leader was elected but only minority alive")
+	} else {
+		fmt.Println("SUCCESS: No leader was elected with minority nodes")
+	}
+}
+
+// Test to make sure follower failure does not cause an election change
+func TestFollowerFailure(t *testing.T) {
+	fmt.Println("Running:", t.Name())
+
+	os.Setenv("RAFT_HEARTBEAT_INTERVAL", "500")
+	os.Setenv("RAFT_ELECTION_TIMEOUT_MIN", "1000")
+	os.Setenv("RAFT_ELECTION_TIMEOUT_MAX", "2000")
+
+	// test node configuration
+	peers := map[int32]string{
+		0: "localhost:50071",
+		1: "localhost:50072",
+		2: "localhost:50073",
+	}
+
+	nodes := make([]*raft.RaftNode, 3)
+
+	for i := range 3 {
+		shutdown := make(chan struct{})
+		nodes[i] = raft.NewRaftNode(int32(i), peers, shutdown)
+		go utils.ServeBackend(int32(i), peers, shutdown, nodes[i])
+	}
+
+	// start status checking
+	statusChan := make(chan struct{})
+	statusUpdates := checkAllStatus(nodes, 100*time.Millisecond, statusChan)
+
+	// wait for initial stable leader election
+	leader := waitForStableLeader(statusUpdates, 10*time.Second)
+	if leader == -1 {
+		t.Fatal("FAILURE: could not achieve stable leadership in 10 seconds")
+	}
+	fmt.Printf("Initial leader: %d\n", leader)
+
+	// find a follower to kill
+	var followerToKill int
+	for i := range 3 {
+		if i != leader {
+			followerToKill = i
+			break
+		}
+	}
+
+	// shut down a follower
+	close(nodes[followerToKill].Shutdown)
+	fmt.Printf("Follower %d shut down\n", followerToKill)
+
+	// wait a bit for cluster to stabilize
+	time.Sleep(3 * raft.DefaultRPCTimeout * time.Second)
+
+	// leader should remain the same
+	stableLeader := waitForStableLeader(statusUpdates, 10*time.Second)
+	if stableLeader != leader {
+		t.Errorf("FAILURE: expected leader %d to remain, but got %d", leader, stableLeader)
+	} else {
+		fmt.Printf("Leader %d is still active after follower failure\n", stableLeader)
+	}
+
+	for i := range 3 {
+		if i != followerToKill {
+			close(nodes[i].Shutdown)
 		}
 	}
 }
